c8d3c7eda89e187b84aef0ee2edd7534b2821c4a
Jacob Weidokal <jweidokal@yahoo.com>
2020-03-19 19:16:01 +0000

    changes

---
+++ EmployeesApi/Controllers/EmployeesController.cs
@@ -0,0 +1,39 @@
+﻿using EmployeesApi.Domain;
+using EmployeesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        EmployeesDataContext Context;
+
+        public EmployeesController(EmployeesDataContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet("/employees")]
+        public ActionResult GetEmployees()
+        {
+            var model = new EmployeesGetResponse();
+
+            var response = Context.Employees
+                .Where(e => e.IsActive)
+                .Select(e => new EmployeesGetResponseItem
+                {
+                    Id = e.Id,
+                    FirstName = e.FirstName,
+                    LastName = e.LastName,
+                    Department = e.Department
+                })
+                .ToList();
+            model.Data = response;
+            return Ok(model);
+        }
+    }
+}
--- EmployeesApi/Startup.cs
+++ EmployeesApi/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeesApi.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,11 @@ namespace EmployeesApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors();
+            services.AddDbContext<EmployeesDataContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("EmployeesDatabase"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +41,13 @@ namespace EmployeesApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(options =>
+            {
+                options.AllowAnyOrigin();
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
 
             app.UseRouting();
 
